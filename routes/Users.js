1e480114356c5b9a37266eb385bb1698a34754d5
Aldrin Sulit <aljersulit12@gmail.com>
2023-02-10 11:50:03 +0800

    added async/await on routes

--- routes/Users.js
+++ routes/Users.js
@@ -2,14 +2,14 @@ const router = require("express").Router();
 const User = require("../models/Userschema");
 
 // READ
-router.get("/", (req, res) => {
-  User.find()
+router.get("/", async (req, res) => {
+  await User.find()
     .then((result) => res.json(result))
     .catch((err) => res.status(400).send(err));
 });
 
 // CREATE
-router.post("/add", (req, res) => {
+router.post("/add", async (req, res) => {
   const { fullname, temperature, email, phonenumber } = req.body;
   const newUser = new User({
     fullname,
@@ -18,29 +18,29 @@ router.post("/add", (req, res) => {
     phonenumber,
   });
 
-  newUser
+  await newUser
     .save()
     .then((result) => res.json(result))
     .catch((err) => res.status(400).send(err));
 });
 
 // DETAILS
-router.get("/:id", (req, res) => {
-  User.findById(req.params.id)
+router.get("/:id", async (req, res) => {
+  await User.findById(req.params.id)
     .then((result) => res.json(result))
     .catch((err) => res.status(400).send(err));
 });
 
 // DELETE
-router.delete("/delete/:id", (req, res) => {
-  User.deleteOne({ _id: req.params.id })
+router.delete("/delete/:id", async (req, res) => {
+  await User.deleteOne({ _id: req.params.id })
     .then((result) => res.json(result))
     .catch((err) => res.status(400).send(err));
 });
 
 // UPDATE
-router.put("/update/:id", (req, res) => {
-  User.updateOne({ _id: req.params.id }, { $set: { ...req.body } })
+router.put("/update/:id", async (req, res) => {
+  await User.updateOne({ _id: req.params.id }, { $set: { ...req.body } })
     .then((result) => res.json(result))
     .catch((err) => res.status(400).send(err));
 });
